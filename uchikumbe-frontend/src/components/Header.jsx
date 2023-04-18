a4db4e4d505bba648f9012d0e3042ccc31852f72
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-04-18 06:51:44 +0200

    Updated header

--- uchikumbe-frontend/src/components/Header.jsx
+++ uchikumbe-frontend/src/components/Header.jsx
@@ -4,15 +4,15 @@ import logo from "../assets/logo.svg"
 const Header = () => {
   return (
     <header className="bg-green-900 text-white">
-      <div className="container mx-auto flex items-center justify-between py-4">
+      <div className="container mx-auto flex ml-1.5 mr-1.5 items-center justify-between py-4">
         <div className="flex items-center">
           
             <img className="h-13 mr-2" src={logo} alt="Logo" />
            
-          <h1 className="font-bold text-xl">Uchikumbe</h1>
+          <h1 className="font-bold  text-xl">Uchikumbe</h1>
         </div>
-        <div className="hidden md:block">
-          <form className="mr-4">
+        <div className="hidden flex md:block">
+          <form className=" mb-6 mr-4">
             <input
               type="text"
               placeholder="Search..."
@@ -23,24 +23,24 @@ const Header = () => {
             </button>
           </form>
           <nav>
-            <ul className="flex">
+            <ul className="  flex">
               <li className="mr-6">
                 <a href="#" className="hover:text-gray-400">Home</a>
               </li>
               <li className="mr-6">
-                <a href="#" className="hover:text-gray-400">About</a>
+                <a href="#" className="hover:text-gray-400">People</a>
               </li>
               <li className="mr-6">
-                <a href="#" className="hover:text-gray-400">Blog</a>
+                <a href="#" className="hover:text-gray-400">News</a>
               </li>
               <li className="mr-6">
-                <a href="#" className="hover:text-gray-400">Contact</a>
+                <a href="#" className="hover:text-gray-400">Forums</a>
               </li>
             </ul>
           </nav>
         </div>
-        <div>
-          <a href="#" className="mr-2 hover:text-gray-400">Sign In</a>
+        <div className="">
+          <a href="#" className="mr-2 hover:text-gray-400">Login</a>
           <a href="#" className="bg-green-500 text-white rounded-md py-1 px-2 hover:bg-green-600">Sign Up</a>
         </div>
       </div>
