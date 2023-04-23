8b222920542bed7cf276e478ca5a3dff13586090
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-04-23 09:40:42 +0200

    card update

--- uchikumbe-frontend/src/components/Home.jsx
+++ uchikumbe-frontend/src/components/Home.jsx
@@ -4,54 +4,18 @@ const Home= () => {
     <div className="p-4 gap-2  flex">
 
       
-      <div className="flex-grow rounded-xl rou bg-green-100">
+      <div className="flex-wrap ml-2 justify-center md:ml-20 flex rounded-xl rou bg-green-100">
         
       <Posts/>
+      <Posts />
+      <Posts />
 
-        <div className="p-4">
-          <h1 className="text-lg font-semibold">Cards</h1>
-          <p>olor, sit amet consectetur adipisicing elit. 
-            Odio, dicta rerum exercitationem, nostrum deleniti quis nesciunt ratione delectus 
-            tenetur molestias velit inventore maxime, voluptatum error cum ex voluptates assumenda 
-            quae. section goes Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis eveniet dolore est
-             unde magni similique delectus! Voluptatem odio rerum autem porro id quasi repudiandae? Maiores
-              excepturi optio et hic commodi</p>
-        </div>
-
-        <div className="p-4">
-          <h1 className="text-lg font-semibold">Cards</h1>
-          <p>olor, sit amet consectetur adipisicing elit. 
-            Odio, dicta rerum exercitationem, nostrum deleniti quis nesciunt ratione delectus 
-            tenetur molestias velit inventore maxime, voluptatum error cum ex voluptates assumenda 
-            quae. section goes Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis eveniet dolore est
-             unde magni similique delectus! Voluptatem odio rerum autem porro id quasi repudiandae? Maiores
-              excepturi optio et hic commodi</p>
-        </div>
-
-        <div className="p-4">
-          <h1 className="text-lg font-semibold">Cards</h1>
-          <p>olor, sit amet consectetur adipisicing elit. 
-            Odio, dicta rerum exercitationem, nostrum deleniti quis nesciunt ratione delectus 
-            tenetur molestias velit inventore maxime, voluptatum error cum ex voluptates assumenda 
-            quae. section goes Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis eveniet dolore est
-             unde magni similique delectus! Voluptatem odio rerum autem porro id quasi repudiandae? Maiores
-              excepturi optio et hic commodi</p>
-        </div>
-
-        <div className="p-4">
-          <h1 className="text-lg font-semibold">Cards</h1>
-          <p>olor, sit amet consectetur adipisicing elit. 
-            Odio, dicta rerum exercitationem, nostrum deleniti quis nesciunt ratione delectus 
-            tenetur molestias velit inventore maxime, voluptatum error cum ex voluptates assumenda 
-            quae. section goes Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis eveniet dolore est
-             unde magni similique delectus! Voluptatem odio rerum autem porro id quasi repudiandae? Maiores
-              excepturi optio et hic commodi</p>
-        </div>
+    
 
       </div>
       
       {/* Left Section */}
-      <div className="flex-shrink-0 w-1/4 w-80 rounded-xl">
+      <div className="flex-shrink-0 w-1/4 w-80 hidden md:block rounded-xl">
 
        
         <div className="p-4">
--- uchikumbe-frontend/src/components/Posts.jsx
+++ uchikumbe-frontend/src/components/Posts.jsx
@@ -14,29 +14,38 @@ import {
 
 import Picture from "../assets/insect.jpg"
 import Avata from "../assets/avatar.png"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Posts() {
     return (
-        <Card className= " min-h-auto flex w-96"> 
+        <Card className= "m-8 h-screen flex min-w-[300px] w-96"> 
             <CardHeader floated={false} className = "shadow-none h-15 min-h-auto">
-                    <div className="flex space-x-2">
-                        <Avatar src={Avata} alt = "avatar" variant="circular"></Avatar>
-                        <div className=" ">
+                <div className="flex space-x-2">
+                    <Avatar src={Avata} alt = "avatar" variant="circular"></Avatar>
+                    <div className=" ">
                         <Typography color="blue-gray" className="mb-2 text-lg ">
-                        Emma Limbe
+                                Emma Limbe
                         </Typography>
                         <Typography color="gray" className="text-xs  " textGradient>
-                        33m ago
+                                33m ago
                         </Typography>
                     </div>
-                   
 
+                    <div className="justify-end mr-8">
+                          <IconButton variant="text" >
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    </IconButton>
+                    </div>
+                   
                 </div>
+               
+
             </CardHeader>
             <CardBody className="h-80 min-h-auto">
                 <div>
                     <p>Lorem ip. Itaque lnam accusamus, ea repellat inventore facere quisquam sit?</p>
-                    <img src={Picture}/>
+                    <img src={Picture} className="flex"/>
                 </div>
             </CardBody>
         </Card>
