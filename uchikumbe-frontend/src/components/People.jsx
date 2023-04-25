0dfff282227470a42a9711131b6ef95d58deb20c
Jimmy Maloya <61555015+Jim-1my@users.noreply.github.com>
2023-04-25 13:40:39 -0700

    people added

--- uchikumbe-frontend/src/components/People.jsx
+++ uchikumbe-frontend/src/components/People.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
+import { CardHeader, Section, Card, CardBody, H5, LeadText, Icon, IconButton } from '@material-tailwind/react';
+import { Avatar } from '@material-tailwind/react';
+import avatar from '../assets/avatar.png';
+import { 
+  faComment, 
+  faUser,
+  faEnvelope, 
+  faThumbsUp} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const People = () => {
   return (
-    <div className="bg-blue-200 h-screen">
-      <h1 className="text-3xl font-bold text-center mt-16">People Component</h1>
-      <p>
+    <Card className="p-4">
+    <div>
+      <h1 className="text-3xl font-large text-green-900 pr-10">Members</h1>
+      <div className=" w-50 h-1/2 ">
+        <div className="bg-white overflow-hidden shadow rounded-lg ">
+          <div className="sm:p-6 w-full border-0 bg-white shadow-md rounded-md p-4 border-t border-b ">
+             <div className="flex items-center space-x-2">
+              <Avatar src={avatar} size="sm" />
+              <span className="text-lg leading-6 font-medium text-gray-900">Jimmy Maloya {}</span>
+         </div>
+        <div className=" flex justify-end p-1/2">
+                    <IconButton variant="text" color="green" size="sm">
+                      
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    </IconButton>
+                    </div>
+         
+             </div>
 
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, aperiam minus asperiores libero natus deleniti praesentium animi laboriosam rerum dolores voluptates quas eveniet omnis. Eos rerum dolorem officia. Nihil, eum.
-      </p>
+             <div className="sm:p-6 w-full border-0 bg-white shadow-md rounded-md p-4 border-t border-b ">
+             <div className="flex items-center space-x-2">
+              <Avatar src={avatar} size="sm" />
+              <span className="text-lg leading-6 font-medium text-gray-900">Jimmy Maloya {}</span>
+         </div>
+        <div className=" flex justify-end p-1/2">
+                    <IconButton variant="text" color="green" size="sm">
+                      
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    </IconButton>
+                    </div>
+         
+             </div>
+              </div>
+          </div>
+          
     </div>
+  </Card>
   );
 };
 
