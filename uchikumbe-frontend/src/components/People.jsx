5413c99fa821adee50bb3c842546a92313ea1012
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-04-26 00:49:55 +0200

    ui updates

--- uchikumbe-frontend/src/components/People.jsx
+++ uchikumbe-frontend/src/components/People.jsx
@@ -9,13 +9,23 @@ const People = () => {
   return (
         <div className="p-4 lg:mx-10 mt-2">
         <h1 className="text-3xl text-center p-4 mt-2 font-large text-green-900 pr-10">Members</h1>
-            <div className="sm:p-6 w-full border  bg-gray-100 shadow-sm border-green-200 rounded-lg p-4 ">
-              <div className="flex items-center space-x-2">
-                <Avatar src={avatar} size="lg" />
-                <span className="text-lg  font-medium text-green-800">
+            <div className="sm:p-6 w-full border flex justify-between bg-white shadow-lg border-green-200 rounded-lg p-4 ">
+              <div className=" justify-center space-x-2">
+                <Avatar src={avatar} variant="circular" size="lg" className="ml-8" />
+                <h3 className="text-xl  text-green-800">
                   Jimmy Maloya 
+                </h3>
+              </div>
+
+              <div className="items-center  md:px-20 border-x-2 px-8 border-green-400">
+                <span>
+                  <h3> <b>Location:</b>  Mulanje</h3>
+                </span>
+                <span className="gap-y-4">
+                   <h3> <b>  Description: </b>Pig farmer</h3>
                 </span>
               </div>
+
               <div className=" flex justify-end ">
                 <IconButton variant="text" color="green" size="lg">
                   <FontAwesomeIcon icon={faEnvelope} />
