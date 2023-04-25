6171fa44fccb1e28708e5090762c66437ebbc7d6
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-04-25 19:02:47 +0200

    refactoring and ui updates

--- uchikumbe-frontend/src/others/NotFound.jsx
+++ uchikumbe-frontend/src/others/NotFound.jsx
@@ -3,7 +3,7 @@ import Img from "../assets/Img.svg"
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-green-100">
+    <div className="flex flex-col p-4 items-center justify-center h-screen bg-green-100">
       <img src={Img} alt="Not found" className='mt-2 h-40'/>
       <h1 className="text-6xl font-bold text-gray-800">Page Not Found</h1>
       <p className="text-gray-800 mt-4">The page you are looking for does not exist.</p>
