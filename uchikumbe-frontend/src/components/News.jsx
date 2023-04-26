e318736973f27b92680bd4fb375928c77b0656e3
Jimmy Maloya <61555015+Jim-1my@users.noreply.github.com>
2023-04-26 01:38:55 -0700

    add on people

--- uchikumbe-frontend/src/components/News.jsx
+++ uchikumbe-frontend/src/components/News.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const News = () => {
   return (
-    <div className="bg-green-200 h-screen">
-      <h1 className="text-3xl font-bold text-center mt-4">News Component</h1>
+    <div className="bg-yellow-200 h-screen">
+      <h1 className="text-3xl font-bold text-center mt-16">News Component</h1>
     </div>
   );
 };
