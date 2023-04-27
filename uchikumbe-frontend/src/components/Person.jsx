beee48fa081a1a49a5543ff1c3e2947de77d217e
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-04-28 00:22:15 +0200

    Update Person.jsx

--- uchikumbe-frontend/src/components/Person.jsx
+++ uchikumbe-frontend/src/components/Person.jsx
@@ -15,7 +15,6 @@ function Person(props){
       
         <div className="bg-white  rounded-lg p-4">
             <div className="flex items-center justify-between mb-2">
-                <h3 className="text-lg font-sans font-semibold text-goldenrod  px-40">{props.title}</h3>
             </div>
             <div className=" px-40 ">
                 <div className="flex items-center my-2 space-x-4 border-b border-grey-500">
@@ -24,7 +23,8 @@ function Person(props){
                         <h4 className="text-lg font-medium text-uchiGreen">{props.name}</h4>
                         <p className="text-sm mb-1 text-goldenrod">{props.description}</p>
                     </div>
-                    <div className="ml-auto">
+                    <div className=" flex items-center ml-auto">
+                        <p className="text-goldenrod mr-2">{props.title}</p>
                         <Link to="/message">
                             <IconButton variant="text" color="green" size="xl" className="">
                                 <FontAwesomeIcon icon={faEnvelope} />
