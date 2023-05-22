9847e013bf2671ef147e3a4742ed140d952a0575
Jimmy Maloya <61555015+Jim-1my@users.noreply.github.com>
2023-05-22 07:54:51 +0200

    news update

--- uchikumbe-frontend/src/components/News.jsx
+++ uchikumbe-frontend/src/components/News.jsx
@@ -11,24 +11,23 @@ import {
   CardBody,
   CardFooter,
   Typography,
+  CardHeader,
   Button 
 } from "@material-tailwind/react";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+
+const randomImage = "https://source.unsplash.com/800x600/?farm,farm-animals";
  
 export default function News() {
   const data = [
     {
-      label: "Crops",
-      value: "crops",
+      label: "General News",
+      value: "General News",
       desc: `It really matters and then like it really doesn't matter.
       What matters is the people who are sparked by it. And the people 
       who are like offended by it, it doesn't matter.`,
     },
-    {
-      label: "Livestock and Poultry",
-      value: "Livestock and Poultry",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
+   
     {
       label: "Agribusiness and Markets",
       value: "Agribusiness and Markets",
@@ -54,7 +53,7 @@ export default function News() {
 return (
  
 <Tabs value="html">
-  <TabsHeader className="bg-transparent pt-4">
+  <TabsHeader className="bg-transparent pt-4 ">
     {data.map(({ label, value }) => (
       <Tab key={value} value={value}>
         {label}
@@ -65,76 +64,105 @@ return (
     {data.map(({ value, desc }) => (
       <TabPanel key={value} value={value}>
        
-        
-       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1">
+        <div className="flex justify-center">
+              <Card className="w-full max-w-[48rem]">
+                <CardHeader
+                  shadow={false}
+                  floated={false}
+                  className="w-2/5 shrink-0 m-0 rounded-r-none"
+                >
+                  <img
+              src={randomImage}
+                    alt="image"
+                    className="w-full h-full object-cover"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="h6" color="green" className="uppercase mb-4">
+                    startups
+                  </Typography>
+                  {desc}
+                  <Typography color="gray" className="font-normal mb-8">
+                    Like so many organizations these days, Autodesk is a company in transition.
+                    It was until recently a traditional boxed software company selling licenses.
+                    Yet its own business model disruption is only part of the story
+                  </Typography>
+                  <a href="#" className="inline-block ">
+                    <Button variant="text" className="flex items-center gap-2" color="green">
+                      Read More
+                      <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
+                    </Button>
+                  </a>
+                </CardBody>
+              </Card>
+            </div>
+<div className="flex justify-center pt-4">
+              <Card className="w-full max-w-[48rem]">
+                <CardHeader
+                  shadow={false}
+                  floated={false}
+                  className="w-2/5 shrink-0 m-0 rounded-r-none"
+                >
+                  <img
+              src={randomImage}
+                    alt="image"
+                    className="w-full h-full object-cover"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="h6" color="green" className="uppercase mb-4">
+                    startups
+                  </Typography>
+                  {desc}
+                  <Typography color="gray" className="font-normal mb-8">
+                    Like so many organizations these days, Autodesk is a company in transition.
+                    It was until recently a traditional boxed software company selling licenses.
+                    Yet its own business model disruption is only part of the story
+                  </Typography>
+                  <a href="#" className="inline-block">
+                    <Button variant="text" className="flex items-center gap-2" color="green" >
+                      Read More
+                      <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
+                    </Button>
+                  </a>
+                </CardBody>
+              </Card>
+            </div> 
+            <div className="flex justify-center pt-4">
+              <Card className="w-full max-w-[48rem]">
+                <CardHeader
+                  shadow={false}
+                  floated={false}
+                  className="w-2/5 shrink-0 m-0 rounded-r-none"
+                >
+                  <img
+              src={randomImage}
+                    alt="image"
+                    className="w-full h-full object-cover"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="h6" color="green" className="uppercase mb-4">
+                    startups
+                  </Typography>
+                  {desc}
+                  <Typography color="gray" className="font-normal mb-8">
+                    Like so many organizations these days, Autodesk is a company in transition.
+                    It was until recently a traditional boxed software company selling licenses.
+                    Yet its own business model disruption is only part of the story
+                  </Typography>
+                  <a href="#" className="inline-block">
+                    <Button variant="text" className="flex items-center gap-2" color="green">
+                      Read More
+                      <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
+                    </Button>
+                  </a>
+                </CardBody>
+              </Card>
+            </div>    
+   
 
-    <Card className="mt-6 w-45">
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          {desc}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0 hover:bg-none">
-        <Button className="bg-green-600">Read More</Button >
-      </CardFooter>
-    </Card>
-    <Card className="mt-6 w-45">
-      <CardBody>
-        <Typography variant="h5" color="blue-gray"  className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          {desc}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-green-600">Read More</Button >
-      </CardFooter>
-    </Card>
-    <Card className="mt-6 w-45">
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          {desc}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-green-600">Read More</Button>
-      </CardFooter>
-    </Card>
-    <Card className="mt-6 w-45">
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          {desc}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-green-600">Read More</Button >
-      </CardFooter>
-    </Card>
-    <Card className="mt-6 w-45">
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          {desc}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-green-600">Read More</Button>
-      </CardFooter>
-    </Card>
-
-    </div>
+    
       </TabPanel>
     ))}
   </TabsBody>
