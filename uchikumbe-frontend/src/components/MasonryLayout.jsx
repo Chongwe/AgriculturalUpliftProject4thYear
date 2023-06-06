8502acefd5d2b47f9039fdc7bbeaeb654fe38c6d
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-06-06 22:00:52 +0200

    ui updates

--- uchikumbe-frontend/src/components/MasonryLayout.jsx
+++ uchikumbe-frontend/src/components/MasonryLayout.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import Masonry from 'react-masonry-css'
 import Posts from './Posts'
+// import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+
+
+
+
 
 const breakPointObj = {
     default: 4,
     1200: 3,
-    1000: 2,
-    500:  1,
+    992: 3,
+    768:  2,
+    576: 1
+   
 }
+
+// const MasonryLayout = ({ posts }) => {
+//   return (
+//    <ResponsiveMasonry 
+//    columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}} > 
+//    <Masonry>
+//           {posts?.map((post) => <Posts key={post._id} post={post} className="w-max"/>)}
+
+//    </Masonry>
+//    </ResponsiveMasonry>
+//   )
+// }
+
 
 const MasonryLayout = ({ posts }) => {
   return (
    <Masonry 
-   className="flex sm:flex-col md:flex-row w- annimate-slide-fwd" 
+   className="flex sm:flex-col md:flex-row  annimate-slide-fwd" 
    breakPointCols={breakPointObj} > 
       {posts?.map((post) => <Posts key={post._id} post={post} className="w-max"/>)}
    </Masonry>
