fecb7ddea95ee9c48935dcad9937fa6026708157
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-06-07 19:22:52 +0200

    input validations

--- uchikumbe-frontend/src/components/Tools.jsx
+++ uchikumbe-frontend/src/components/Tools.jsx
@@ -7,62 +7,61 @@ import {
   TabPanel,
 } from "@material-tailwind/react";
 import {
-  Square3Stack3DIcon,
-  UserCircleIcon,
-  Cog6ToothIcon,
+   RssIcon,
+  BeakerIcon,
+  TrashIcon,
 } from "@heroicons/react/24/solid";
-import{
+import {
   FeedCalculator,
   SeedCalculator,
 } from "../utils/Calculators";
 
-const feedCalculator = <FeedCalculator/>
-const seedCalculator = <SeedCalculator />
- 
+
+
+const feedCalculator = <FeedCalculator />;
+const seedCalculator = <SeedCalculator />;
+
+
 export default function Example() {
   const data = [
     {
-      label: "FeedCalculator",
+      label: "Feed Calculator",
       value: "feed-calculator",
-      icon: Square3Stack3DIcon,
-      desc: feedCalculator
+      icon:  RssIcon,
+      compontent: feedCalculator
     },
     {
       label: "Seed Calculator",
       value: "seed-calculator",
-      icon: UserCircleIcon,
-      desc: seedCalculator
+      icon: BeakerIcon,
+      compontent: seedCalculator
     },
-    {
-      label: "Settings",
-      value: "settings",
-      icon: Cog6ToothIcon,
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
+   
   ];
+
+  const initialValue = data[0].value; // Set the initial value to the value of the first tab
+
   return (
     <div className="h-screen p-2 m-4">
-        <Tabs value="dashboard" orientation="vertical">
-          <TabsHeader className="w-60">
-            {data.map(({ label, value, icon }) => (
-              <Tab key={value} value={value} className="place-items-start">
-                <div className="flex items-center gap-2">
-                  {React.createElement(icon, { className: "w-5 h-5" })}
-                  {label}
-                </div>
-              </Tab>
-            ))}
-          </TabsHeader>
-          <TabsBody>
-            {data.map(({ value, desc }) => (
-              <TabPanel key={value} value={value} className="py-0">
-                {desc}
-              </TabPanel>
-            ))}
-          </TabsBody>
-        </Tabs>
+      <Tabs value={initialValue} orientation="vertical">
+        <TabsHeader className="w-60">
+          {data.map(({ label, value, icon }) => (
+            <Tab key={value} value={value} className="place-items-start">
+              <div className="flex items-center gap-2">
+                {React.createElement(icon, { className: "w-5 h-5" })}
+                {label}
+              </div>
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody>
+          {data.map(({ value, compontent }) => (
+            <TabPanel key={value} value={value} className="py-0">
+              {compontent}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
     </div>
   );
 }
