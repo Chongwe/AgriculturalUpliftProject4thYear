c4074ce7e6d8d78f6c9e72fab44e9eb5932134a0
Bsc-inf-13-18 <128453903+Bsc-inf-13-18@users.noreply.github.com>
2023-06-08 16:05:42 -0700

    added feed calculator for chicken

--- uchikumbe-frontend/src/components/Tools.jsx
+++ uchikumbe-frontend/src/components/Tools.jsx
@@ -17,11 +17,15 @@ import {
 } from "../utils/Calculators";
 import CompostCalculator from "../utils/CompostCalculator";
 import MaizeFertilizerCalculator from "../utils/MaizeFertilizerCalculator"
+import ChickenFeedCalculator from "../utils/ChickenFeedCulculator";
+
+
 
 const feedCalculator = <FeedCalculator />;
 const seedCalculator = <SeedCalculator />;
 const maizeCalculator = <MaizeFertilizerCalculator/>
 const copostCalculator = <CompostCalculator />
+const chickenFeedCalculator = <ChickenFeedCalculator/> 
 
 export default function Example() {
   const data = [
@@ -48,6 +52,12 @@ export default function Example() {
       value: "maize-calculator",
       icon: Cog6ToothIcon,
       desc: maizeCalculator
+    },
+    {
+      label: "Chicken feed Calculator",
+      value: "chickenfeed-calculator",
+      icon: Cog6ToothIcon,
+      desc: chickenFeedCalculator
     },
   ];
 
