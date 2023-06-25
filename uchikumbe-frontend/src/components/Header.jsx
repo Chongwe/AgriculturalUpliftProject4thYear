c00573c4df64ecbd94f3fe889b843305aa4754de
RonexHD <73313132+RonexHD@users.noreply.github.com>
2023-06-25 08:12:35 +0200

    Update Header.jsx

--- uchikumbe-frontend/src/components/Header.jsx
+++ uchikumbe-frontend/src/components/Header.jsx
@@ -120,7 +120,7 @@ export default function Fun({ user = null }) {
           ripple={true}
           onClick={() => setOpenNav(!openNav)}
         >
-          <FontAwesomeIcon className=" " icon={faBars} />
+          <FontAwesomeIcon className="h-5" icon={faBars} />
         </IconButton>
       </div>
       <Collapse open={openNav}>
