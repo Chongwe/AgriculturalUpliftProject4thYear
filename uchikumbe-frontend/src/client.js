f3b0b5baab60be66b32667f7e16a95794c3c4933
Chongwe <bsc-inf-05-18@cc.ac.mw>
2023-06-29 04:46:10 +0200

    updated sanity.client and set useCdn to false

--- uchikumbe-frontend/src/client.js
+++ uchikumbe-frontend/src/client.js
@@ -1,11 +1,11 @@
-import sanityClient from "@sanity/client";
+import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const client = sanityClient({
+export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2021-11-16",
-  useCdn: true,
+  useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
