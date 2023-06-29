ac3961f2babd8fc67aa3ac1326f08f2ded793678
Chongwe <bsc-inf-05-18@cc.ac.mw>
2023-06-29 18:04:58 +0200

    added admin staffhold pages and new sanity io desk

---
+++ uchikumbe-frontend/src/pages/AdminPages/AdminDashboard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+
+import ManageForumCreationRequestPage from "./ManageForumCreationRequestPage";
+import ManageUserPage from "./ManageUserPage";
+
+const manageForumCreationRequest = <ManageForumCreationRequestPage />;
+const manageUser = <ManageUserPage />;
+
+const AdminDashboard = () => {
+  const [activeTab, setActiveTab] = React.useState("html");
+  const data = [
+    {
+      label: "Manage Forum Creation Submission",
+      value: "forum-request-submissions",
+      desc: manageForumCreationRequest,
+    },
+    {
+      label: "Manage Users",
+      value: "manage-users",
+      desc: manageUser,
+    },
+  ];
+
+  const initialValue = data[0].value; // Set the initial value to the value of the first tab
+
+  return (
+    <div className="flex min-w-screen-sm items-center justify-center">
+      <div className="mb-96  flex items-center bg-green-100 rounded-xl p-2 m-4">
+        <Tabs value={initialValue}>
+          <TabsHeader
+            className="rounded-xl bg-white p-4 border-green-900  "
+            indicatorProps={{
+              className:
+                "bg-transparent border-t-2 border-green-500 shadow-none rounded-none",
+            }}
+          >
+            {data.map(({ label, value }) => (
+              <Tab
+                key={value}
+                value={value}
+                onClick={() => setActiveTab(value)}
+                className={activeTab === value ? "text-green-500" : ""}
+              >
+                {label}
+              </Tab>
+            ))}
+          </TabsHeader>
+          <TabsBody>
+            {data.map(({ value, desc }) => (
+              <TabPanel key={value} value={value}>
+                {desc}
+              </TabPanel>
+            ))}
+          </TabsBody>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ uchikumbe-frontend/src/pages/AdminPages/ManageForumCreationRequestPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManageForumCreationRequestPage = () => {
+  return <div>ManageForumCreationRequestPage</div>;
+};
+
+export default ManageForumCreationRequestPage;
